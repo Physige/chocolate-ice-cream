cd5b6999d773a3ab0d572e44b63752e8539e8c0b
physige <niaar2004@gmail.com>
2022-03-08 11:52:10 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        "GoboldBold":"Gobold Bold",
+        "GoboldHollowBold":"Gobold Hollow Bold",
+        "GoboldHollow":"Gobold Hollow",
+        "LEMONMILK":"LEMONMILK",
+      }
+    },
+  },
+  plugins: [],
+}
