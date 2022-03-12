1fd8491367ba356fe944bba7804871216d35ec62
physige <niaar2004@gmail.com>
2022-03-11 19:53:27 -0500

    added sticky video

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 import './index.css'
+
+const video = document.getElementById("video2"); 
+var time = 0;
+
+document.onscroll = () => {
+    const clientHeight = document.documentElement.clientHeight;
+    const videoYPos = video.getBoundingClientRect().y;
+    
+    // checks when viewport reaches the sticky video
+    if (clientHeight > videoYPos + videoYPos * 2/3) {
+        // runs through frames of video
+        time += 1/72
+        video.currentTime = (time);
+    }
+
+}
+
